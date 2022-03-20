53d334f0dc13129ee9ae345035e91b01c0dd2cd5
jrio95 <47417232+jrio95@users.noreply.github.com>
2022-03-20 15:44:19 +0100

    Added service layer

---
+++ src/Lw.Service/Lw.Service/Services/TranslateService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lw.Service.Services
+{
+    public class TranslateService : ITranslateService
+    {
+    }
+}
