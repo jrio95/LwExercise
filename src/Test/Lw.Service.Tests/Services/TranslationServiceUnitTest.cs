45c48996e56d2aa9dc69541100310dd62ac26e09
jrio95 <47417232+jrio95@users.noreply.github.com>
2022-03-25 22:42:21 +0100

    Translation service unit test finished

--- src/Test/Lw.Service.Tests/Services/TranslationServiceUnitTest.cs
+++ src/Test/Lw.Service.Tests/Services/TranslationServiceUnitTest.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Lw.Domain.Entities;
 using Lw.DTO.DTOs;
+using Lw.DTO.Enums;
 using Lw.DTO.Mappings;
 using Lw.Repository.Repositories;
 using Lw.Service.Services;
@@ -15,7 +16,7 @@ using System.Threading.Tasks;
 namespace Lw.API.Tests.Controllers
 {
     [TestClass]
-    public class TranslationRepositoryUnitTest
+    public class TranslationServiceUnitTest
     {
         private ITranslationService _translationService;
         private ITranslationRepository _translationRepositoryMock;
@@ -27,7 +28,7 @@ namespace Lw.API.Tests.Controllers
         {
             var translationRepositoryMock = new Mock<ITranslationRepository>();
             translationRepositoryMock.Setup(x => x.GetTranslationListBySentenceId(It.IsAny<int>())).Returns(ListTranslationMock());
-            translationRepositoryMock.Setup(x => x.GetTranslationByLanguageIdAndSentenceId(It.IsAny<int>(), It.IsAny<int>())).Returns(TranslationMock());
+            translationRepositoryMock.Setup(x => x.GetTranslationByLanguageIdAndSentenceId((int)LanguageEnum.es_ES, It.IsAny<int>())).Returns(TranslationMock());
 
             return translationRepositoryMock.Object;
         }
@@ -46,7 +47,7 @@ namespace Lw.API.Tests.Controllers
             return new TranslationDTO()
             {
                 TranslationId = 1,
-                LanguageISO = DTO.Enums.LanguageEnum.es_ES,
+                LanguageISO = LanguageEnum.es_ES,
                 TranslatedSentence = "Hola"
             };
         }
@@ -107,6 +108,40 @@ namespace Lw.API.Tests.Controllers
             this._translationRepositoryMock = SetUpTranslationRepository();
             _mapper = SetUpAutoMapper();
             _translationService = new TranslationService(_translationRepositoryMock, _mapper);
+        }
+
+        [TestMethod]
+        public void GetTranslation_WithNoLanguage_ThenReturnTranslationList()
+        {
+            var translationListMock = ListTranslationMock().ToList();
+            var helloSentenceId = 1;
+
+            var translations = _translationService.GetTranslation(null, helloSentenceId);
+            var translationList = translations.ToList();
+
+            Assert.AreEqual(translationList[0].TranslationId, translationListMock[0].TranslationId);
+            Assert.AreEqual((int)translationList[0].LanguageISO, translationListMock[0].LanguageId);
+            Assert.AreEqual(translationList[0].TranslatedSentence, translationListMock[0].TranslatedSentence);
+
+            Assert.AreEqual(translationList[1].TranslationId, translationListMock[1].TranslationId);
+            Assert.AreEqual((int)translationList[1].LanguageISO, translationListMock[1].LanguageId);
+            Assert.AreEqual(translationList[1].TranslatedSentence, translationListMock[1].TranslatedSentence);
+        }
+
+        [TestMethod]
+        public void GetTranslation_WithSpecifiedLanguage_ThenReturnSpecifiedTranslation()
+        {
+            var translationMock = TranslationMock();
+            var helloSentenceId = 1;
+
+            var translationsResult = _translationService.GetTranslation(DTO.Enums.LanguageEnum.es_ES, helloSentenceId);
+            var translationResultList = translationsResult.ToList();
+
+            Assert.IsTrue(translationResultList.Any());
+            Assert.IsTrue(translationResultList.Count == 1);
+            Assert.AreEqual(translationResultList[0].TranslationId, translationMock.TranslationId);
+            Assert.AreEqual((int)translationResultList[0].LanguageISO, translationMock.LanguageId);
+            Assert.AreEqual(translationResultList[0].TranslatedSentence, translationMock.TranslatedSentence);
         }
     }
 }
